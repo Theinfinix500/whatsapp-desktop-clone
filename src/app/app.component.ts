6395e53a590efcf04c04d1221e8c615574a914aa
abdelali djebara <adjebara@sqli.com>
2024-01-18 23:19:15 +0100

    sidemenu icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgIconComponent, NgIconsModule, provideIcons } from '@ng-icons/core';
+import {
+  ionSettingsOutline,
+  ionArchiveOutline,
+  ionStarOutline,
+  ionChatbubbleEllipsesOutline,
+  ionCallOutline,
+  ionPersonCircleOutline
+} from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgIconComponent],
+  providers: [
+    provideIcons({
+      ionSettingsOutline,
+      ionArchiveOutline,
+      ionStarOutline,
+      ionChatbubbleEllipsesOutline,
+      ionCallOutline,
+      ionPersonCircleOutline
+    }),
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'whatsapp-clone';
